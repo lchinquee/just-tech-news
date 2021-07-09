2bf0e4e83e5fea165b7f9ff5e880af6228184e4f
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-07-08 21:40:28 -0400

    add post route

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// Create associations
+// User to multiple posts relationship
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// Post to user relationship
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
